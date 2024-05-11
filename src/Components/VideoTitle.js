5c4ee5b77df6365917764b0f0a9823c1c3b331ce
NarendranS13 <narendran.s13@zohomail.in>
2024-05-11 21:08:49 +0530

    Updating the css for few video title

--- src/Components/VideoTitle.js
+++ src/Components/VideoTitle.js
@@ -6,7 +6,7 @@ const VideoTitle = ({title, overview}) => {
   return (
     <div className='w-screen aspect-video pt-[25%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black'>
         <h1 className='text-lg md:text-6xl font-bold'>{title}</h1>
-        <p className='hidden md:inline-block py-6 text-lg w-1/4'>{overview}</p>
+        <p className='hidden md:inline-block md:py-6 text-lg md:w-1/2'>{overview}</p>
         <div >
             <button className='bg-white text-black md:py-4 px-3 py-2 md:px-12 text-xl my-4 md:mt-0  rounded-md hover:bg-opacity-80'>
             <img src={playIcon} alt="Play Icon" className="w-6 h-6 mr-2 inline-block"/>
